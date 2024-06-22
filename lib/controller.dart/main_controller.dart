8c212c847f151cb7d6916243487757b0b5fff26f
Nadi Pulse <npulseprognostics@gmail.com>
2024-06-22 12:20:20 +0530

    ADD pdf

--- lib/controller.dart/main_controller.dart
+++ lib/controller.dart/main_controller.dart
@@ -1,16 +1,17 @@
+import 'dart:developer' as logger;
+import 'dart:io';
 import 'dart:math';
-
-import 'package:medai/views/screens/recent_chats_screen.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
+import 'package:medai/views/screens/main_screen.dart';
+import 'package:medai/views/screens/recent_chats_screen.dart';
+import 'package:read_pdf_text/read_pdf_text.dart';
 
 class MainController extends GetxController {
   RxInt currentPageIndex = 0.obs;
-  int generateRandomNumber(int min, int max) {
-    Random random = Random();
-    int number = random.nextInt(max - min + 1) +
-        min; // generates a random integer between min and max (inclusive)
-    return number;
-  }
+  RxBool isLoading = false.obs;
 
   @override
   void onInit() {
@@ -18,6 +19,13 @@ class MainController extends GetxController {
     super.onInit();
   }
 
+  int generateRandomNumber(int min, int max) {
+    Random random = Random();
+    int number = random.nextInt(max - min + 1) + min;
+    return number;
+  }
+
+  String pdfContent = "";
   List<PromptObject> promptList = [
     PromptObject(
         prompt:
@@ -31,7 +39,110 @@ class MainController extends GetxController {
         prompt:
             "I want you to act as an AI assisted doctor. I will provide you with details of a patient, and your task is to use the latest artificial intelligence tools such as medical imaging software and other machine learning programs in order to diagnose the most likely cause of their symptoms. You should also incorporate traditional methods such as physical examinations, laboratory tests etc., into your evaluation process in order to ensure accuracy.",
         name: "AI Assisted Doctor"),
+    PromptObject(prompt: '''''', name: "Chat with your report"),
   ];
+
+  Future<bool> pdfPicker() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+    if (result != null) {
+      File file = File(result.files.single.path!);
+      String pdfContent = await getPDFtext(file.path);
+      int estimatedPages = estimatePages(pdfContent);
+      if (estimatedPages < 3) {
+        if (isMedicalContent(pdfContent)) {
+          pdfContent = removeRedundancy(pdfContent);
+          promptList.last = PromptObject(
+            prompt:
+                '''I want you to act as an AI-assisted doctor specializing in patient diagnosis. I will provide you with the extracted text content of a patient's PDF medical report. Your task is to:
+Identify and summarize the patient's basic information (name, age, gender, collection date, report status, etc.).
+Determine the type of report (e.g., thyroid profile, diabetic panel, lipid profile, etc.).
+Analyze the test results and compare them with the biological reference intervals.
+Incorporate traditional diagnostic methods such as physical examination findings, laboratory tests, and clinical guidelines to ensure the accuracy and comprehensiveness of your evaluation.
+Provide a detailed diagnosis and recommend potential treatment options based on your integrated analysis.
+Example Input: "$pdfContent" ''',
+            name: "Chat with your report",
+          );
+        } else {
+          flutterToast("The selected report does not contain medical content.");
+          isLoading.value = false;
+
+          return false;
+        }
+      } else {
+        flutterToast(
+            'The selected report is too lengthy. Please select a shorter report.');
+        isLoading.value = false;
+
+        return false;
+      }
+    } else {
+      flutterToast('Please select a PDF file to continue.');
+      isLoading.value = false;
+      return false;
+    }
+    return true;
+  }
+
+  Future<String> getPDFtext(String path) async {
+    try {
+      pdfContent = await ReadPdfText.getPDFtext(path);
+      return pdfContent;
+    } on PlatformException {
+      flutterToast(
+          "An error occurred while reading the PDF file. Please try again.");
+      isLoading.value = false;
+
+      await Get.off(() => const MainScreen());
+    }
+    return "";
+  }
+
+  int estimatePages(String text) {
+    const int avgCharsPerPage = 2500;
+
+    return (text.length / avgCharsPerPage).ceil();
+  }
+
+  bool isMedicalContent(String text) {
+    List<String> medicalKeywords = [
+      "patient",
+      "diagnosis",
+      "treatment",
+      "prescription",
+      "test",
+      "results",
+      "lab",
+      "report"
+    ];
+    for (var keyword in medicalKeywords) {
+      if (text.toLowerCase().contains(keyword.toLowerCase())) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  String removeRedundancy(String text) {
+    Set<String> uniqueLines = {};
+    List<String> lines = text.split('\n');
+    for (String line in lines) {
+      uniqueLines.add(line.trim());
+    }
+    return uniqueLines.join('\n');
+  }
+
+  flutterToast(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      fontSize: 16.0,
+    );
+  }
 }
 
 class PromptObject {
--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,9 +1,11 @@
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:medai/controller.dart/main_controller.dart';
 import 'package:medai/controller.dart/recent_chat_controller.dart';
 import 'package:medai/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:medai/views/screens/loading_screen.dart';
 
 import '../../controller.dart/chat_controller.dart';
 
@@ -16,6 +18,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   RecentChatController recentChatController = Get.put(RecentChatController());
+  MainController mainController = Get.find<MainController>();
   final timeFormat = DateFormat('MMMM d');
 
   @override
@@ -32,136 +35,189 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: TextColors.backgroundColor,
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.blueGrey.shade900,
-                    ),
-                    child: Column(
-                      children: [
-                        const Padding(
-                          padding:
-                              EdgeInsets.only(top: 18, left: 10, right: 10),
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "Prompts ",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w700),
+      body: Obx(
+        () => mainController.isLoading.value
+            ? Center(
+                child: SizedBox(
+                height: Get.height,
+                child: LoadingAnimationWidget.fallingDot(
+                    color: Colors.teal, size: 36),
+              ))
+            : SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.blueGrey.shade900,
                             ),
-                          ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.only(
-                              top: 10, left: 10, right: 30, bottom: 13),
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "Play with our chatbot with some famous prompts to get started with.",
-                              style: TextStyle(
-                                  color: Colors.white54,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                          ),
-                        ),
-                        ListView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount:
-                                Get.find<MainController>().promptList.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return Material(
-                                color: Colors.transparent,
-                                child: InkWell(
-                                  onTap: () {
-                                    Get.put(ChatController()).chatBoxId =
-                                        Get.find<MainController>()
-                                            .generateRandomNumber(1, 30000);
-                                    // Get.find<ChatController>().isOldChat.value = false;
-                                    Get.find<ChatController>().chatIndex =
-                                        Get.find<RecentChatController>()
-                                                .allChats
-                                                .isEmpty
-                                            ? 0
-                                            : Get.find<RecentChatController>()
-                                                .allChats
-                                                .length;
-                                    // Get.find<ChatController>()
-                                    Get.find<ChatController>().openHiveBox();
-                                    Get.find<ChatController>()
-                                            .titleTextEditingController
-                                            .text =
-                                        Get.find<MainController>()
-                                            .promptList[index]
-                                            .name;
-
-                                    Get.find<ChatController>()
-                                            .promptDescriptio =
-                                        "${Get.find<MainController>().promptList[index].prompt}  请用 markdown 格式输出。\n";
-                                    Get.find<ChatController>()
-                                        .isPromptChat
-                                        .value = true;
-
-                                    Get.toNamed("/chatScreen")!.then((value) =>
-                                        Get.find<RecentChatController>()
-                                            .getDataFromHive());
-                                    // Get.toNamed("/journalEntry");
-                                  },
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 18,
-                                        bottom: 16,
-                                        left: 10,
-                                        right: 10),
-                                    child: Row(children: [
-                                      Text(
-                                        Get.find<MainController>()
-                                            .promptList[index]
-                                            .name,
-                                        style: const TextStyle(
-                                            color: Colors.white70,
-                                            fontSize: 15,
-                                            fontWeight: FontWeight.w600),
-                                      ),
-                                      const Spacer(),
-                                      const Icon(
-                                        Icons.arrow_forward_ios,
-                                        color: Colors.white70,
-                                        size: 16,
-                                      ),
-                                    ]),
+                            child: Column(
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.only(
+                                      top: 18, left: 10, right: 10),
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      "Prompts ",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w700),
+                                    ),
                                   ),
                                 ),
-                              );
-                            }),
-                        const SizedBox(height: 10),
-                      ],
-                    ),
+                                const Padding(
+                                  padding: EdgeInsets.only(
+                                      top: 10, left: 10, right: 30, bottom: 13),
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      "Play with our chatbot with some famous prompts to get started with.",
+                                      style: TextStyle(
+                                          color: Colors.white54,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ),
+                                ),
+                                ListView.builder(
+                                    shrinkWrap: true,
+                                    physics:
+                                        const NeverScrollableScrollPhysics(),
+                                    itemCount: Get.find<MainController>()
+                                        .promptList
+                                        .length,
+                                    itemBuilder:
+                                        (BuildContext context, int index) {
+                                      return Material(
+                                        color: Colors.transparent,
+                                        child: InkWell(
+                                          onTap: () async {
+                                            bool goForward = false;
+                                            final mainController =
+                                                Get.find<MainController>();
+                                            final chatController =
+                                                Get.put(ChatController());
+                                            final recentChatController = Get
+                                                .find<RecentChatController>();
+
+                                            if (mainController
+                                                .promptList[index].name
+                                                .contains('report')) {
+                                              setState(() {
+                                                mainController.isLoading.value =
+                                                    true;
+                                              });
+                                              goForward = await mainController
+                                                  .pdfPicker();
+                                              if (mainController
+                                                      .promptList.last.name !=
+                                                  "Chat with your report") {
+                                                return;
+                                              }
+                                            } else {
+                                              goForward = true;
+                                            }
+
+                                            chatController.chatBoxId =
+                                                mainController
+                                                    .generateRandomNumber(
+                                                        1, 30000);
+                                            chatController.chatIndex =
+                                                recentChatController
+                                                        .allChats.isEmpty
+                                                    ? 0
+                                                    : recentChatController
+                                                        .allChats.length;
+                                            chatController.openHiveBox();
+
+                                            final prompt = mainController
+                                                .promptList[index];
+                                            chatController
+                                                .titleTextEditingController
+                                                .text = prompt.name;
+                                            chatController.promptDescriptio =
+                                                "${prompt.prompt}  请用 markdown 格式输出。\n";
+                                            chatController.isPromptChat.value =
+                                                true;
+
+                                            if (goForward) {
+                                              mainController.isLoading.value =
+                                                  false;
+                                              Get.toNamed("/chatScreen")!.then(
+                                                (value) => recentChatController
+                                                    .getDataFromHive(),
+                                              );
+                                            }
+                                            mainController.isLoading.value =
+                                                false;
+                                          },
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                                top: 18,
+                                                bottom: 16,
+                                                left: 10,
+                                                right: 10),
+                                            child: Row(children: [
+                                              if (Get.find<MainController>()
+                                                  .promptList[index]
+                                                  .name
+                                                  .contains('report'))
+                                                const Padding(
+                                                  padding: EdgeInsets.only(
+                                                      right: 12),
+                                                  child: Icon(
+                                                    Icons.picture_as_pdf,
+                                                    color: Color.fromARGB(
+                                                        255, 170, 43, 43),
+                                                    size: 20,
+                                                  ),
+                                                ),
+                                              Text(
+                                                Get.find<MainController>()
+                                                    .promptList[index]
+                                                    .name,
+                                                style: const TextStyle(
+                                                    color: Colors.white70,
+                                                    fontSize: 15,
+                                                    fontWeight:
+                                                        FontWeight.w600),
+                                              ),
+                                              const Spacer(),
+                                              const Icon(
+                                                Icons.arrow_forward_ios,
+                                                color: Colors.white70,
+                                                size: 16,
+                                              ),
+                                            ]),
+                                          ),
+                                        ),
+                                      );
+                                    }),
+                                const SizedBox(height: 10),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-              const SizedBox(
-                height: 25,
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
