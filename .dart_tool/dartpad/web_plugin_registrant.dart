eb1ce01bd7266f45e4bdaee6447a8cbaaced809b
Nadi Pulse <npulseprognostics@gmail.com>
2024-06-23 12:31:37 +0530

    fix pubsec file

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,6 +6,7 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:flutter_tts/flutter_tts_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:speech_to_text/speech_to_text_web.dart';
@@ -13,6 +14,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FilePickerWeb.registerWith(registrar);
   FlutterTtsPlugin.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
   SpeechToTextPlugin.registerWith(registrar);
